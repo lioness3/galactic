6f68e015306af4ed452f2d7f2748849518b26d88
lioness3 <jcnailtech3@yahoo.com>
2019-11-15 12:58:22 -0800

    write test to fail

--- __Test__/galactic.test.js
+++ __Test__/galactic.test.js
@@ -1,9 +1,16 @@
 import { AgeCalculator } from './../src/galactic.js';
 
 describe('AgeCalculator', () => {
-  test('Should return users age in Venus years', () => {
-    let venusAge = new AgeCalculator(30,.62);
-    expect(venusAge.planetAgeConversion()).toBeCloseTo(18.6);
+  // test('Should return users age in Venus years', () => {
+  //   let venusAge = new AgeCalculator(30,.62);
+  //   expect(venusAge.planetAgeConversion()).toBeCloseTo(18.6);
+  // });
+  test('Should return users age in mars years', () => {
+    let marsAge = new AgeCalculator(30);
+    expect(marsAge.mars()).toBeCloseTo(56.4);
   });
-
+  test('Should return user  age in jupiter years(rounded)', () => {
+    let jupiterAge = new AgeCalculator(30);
+    expect(jupiterAge.jupiter()).toEqual(3);
+  });
 });
--- src/galactic.js
+++ src/galactic.js
@@ -1,13 +1,40 @@
 export class AgeCalculator{
-  constructor(earthAge,planet){
+  constructor(earthAge){
     this.earthAge = earthAge;
-    this.planet = planet;
   }
 
-  planetAgeConversion() {
-    let planetAge = this.earthAge * this.planet;
+  venus() {
+    let planetAge = this.earthAge * .62;
 
     return planetAge;
   }
 
+
+
 };
+
+
+
+
+
+
+
+
+
+
+// export class AgeCalculator{
+//   constructor(earthAge,planet){
+//     this.earthAge = earthAge;
+//     this.planet = planet;
+//   }
+//
+//   planetAgeConversion() {
+//     let planetAge = this.earthAge * this.planet;
+//
+//     return planetAge;
+//   }
+//
+// const mercury = .24
+// const venus = .62
+// const mars = 1.88
+// const jupiter = 11.86
