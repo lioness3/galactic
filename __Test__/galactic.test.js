a6b34150476d53f03bae68755454025e349cec55
lioness3 <jcnailtech3@yahoo.com>
2019-11-15 11:01:15 -0800

    refactor function for multiple planet inputs and expect test to fail

--- __Test__/galactic.test.js
+++ __Test__/galactic.test.js
@@ -1,11 +1,8 @@
 import { AgeCalculator } from './../src/galactic.js';
 
 describe('AgeCalculator', () => {
-  test('Should return users age in planet years', () => {
-    let planetAge = AgeCalculator(30);
-    expect(planetAge).toBeCloseTo(7.2);
-  });
   test('Should return users age in Venus years', () => {
-    let planetAge = AgeCalculator(30,venus);
-    expect(planetAge).toBeCloseTo(7.2);
+    let planetAge = AgeCalculator(30,.62);
+    expect(planetAge).toBeCloseTo(7.2);//test failed
+  });
 });
