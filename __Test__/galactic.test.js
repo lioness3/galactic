f5a003878e8d032cd72e3e53f82975ab21b65581
lioness3 <jcnailtech3@yahoo.com>
2019-11-15 10:28:01 -0800

    make test fail

--- __Test__/galactic.test.js
+++ __Test__/galactic.test.js
@@ -2,7 +2,7 @@ import { AgeCalculator } from './../src/galactic.js';
 
 describe('AgeCalculator', () => {
   test('Should return users age in planet years', () => {
-    let planetAge = AgeCalculator(earthAge);
+    let planetAge = AgeCalculator(30);
     expect(planetAge).toEqual(2);
 
   });
