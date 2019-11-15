429f09616a6df33bf051b5b1e05b146a571d2578
lioness3 <jcnailtech3@yahoo.com>
2019-11-15 11:03:01 -0800

    write test for code to (pass)

--- __Test__/galactic.test.js
+++ __Test__/galactic.test.js
@@ -3,6 +3,6 @@ import { AgeCalculator } from './../src/galactic.js';
 describe('AgeCalculator', () => {
   test('Should return users age in Venus years', () => {
     let planetAge = AgeCalculator(30,.62);
-    expect(planetAge).toBeCloseTo(18.6);//test failed
+    expect(planetAge).toBeCloseTo(18.6);//test passed
   });
 });
