0f692aac6875112a18f1a4bcbd300d0a573d0f80
lioness3 <jcnailtech3@yahoo.com>
2019-11-15 12:30:43 -0800

    make venus test pass

--- __Test__/galactic.test.js
+++ __Test__/galactic.test.js
@@ -3,7 +3,7 @@ import { AgeCalculator } from './../src/galactic.js';
 describe('AgeCalculator', () => {
   test('Should return users age in Venus years', () => {
     let venusAge = new AgeCalculator(30,.62);
-    expect(venusAge.planetAgeConversion()).toBeCloseTo(5)//expected to fail;
+    expect(venusAge.planetAgeConversion()).toBeCloseTo(18.6);
   });
 
 });
