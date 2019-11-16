a57e55369c54a98550e59a159e533d577c8f9b28
lioness3 <jcnailtech3@yahoo.com>
2019-11-15 16:12:21 -0800

    test passes

--- __Test__/galactic.test.js
+++ __Test__/galactic.test.js
@@ -15,6 +15,6 @@ describe('AgeCalculator', () => {
   });
   test('Should return users age in jupiter years(rounded)', () => {
     let superNova = new AgeCalculator(30);
-    expect(superNova.jupiter()).toEqual(16);
+    expect(superNova.jupiter()).toEqual(3);
   });
 });
